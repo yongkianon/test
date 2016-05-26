734e5fb29098bc93e97cf187a44d63c6335f81a3
yong <yong@alltrix.biz>
2016-05-26 20:57:25 +0800

    hdfs command

--- hadoop/bin/startALL.sh
+++ hadoop/bin/startALL.sh
@@ -1,6 +1,5 @@
 $HADOOP_HOME/sbin/hadoop-daemon.sh start journalnode
 $HADOOP_HOME/sbin/hadoop-daemon.sh start namenode
   $YARN_HOME/sbin/yarn-daemon.sh   start resourcemanager
-  $YARN_HOME/sbin/yarn-daemon.sh   start nodemanager
   $ZKPR_HOME/bin/zkServer.sh       start
 $HADOOP_HOME/sbin/hadoop-daemon.sh start zkfc
--- hadoop/bin/stopALL.sh
+++ hadoop/bin/stopALL.sh
@@ -1,6 +1,5 @@
 $HADOOP_HOME/sbin/hadoop-daemon.sh stop zkfc
   $ZKPR_HOME/bin/zkServer.sh       stop
-  $YARN_HOME/sbin/yarn-daemon.sh   stop nodemanager
   $YARN_HOME/sbin/yarn-daemon.sh   stop resourcemanager
 $HADOOP_HOME/sbin/hadoop-daemon.sh stop namenode
 $HADOOP_HOME/sbin/hadoop-daemon.sh stop journalnode
