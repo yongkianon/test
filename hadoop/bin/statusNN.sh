ea980bf8995ce4538b5994587b7bd242933e340e
yong <yong@alltrix.biz>
2016-06-15 14:42:41 +0800

    hdfs command

---
+++ hadoop/bin/statusNN.sh
@@ -0,0 +1,5 @@
+echo "/usr/bin/hdfs haadmin -getServiceState nn01"
+      /usr/bin/hdfs haadmin -getServiceState nn01
+
+echo "/usr/bin/hdfs haadmin -getServiceState nn02"
+      /usr/bin/hdfs haadmin -getServiceState nn02
---
+++ hadoop/bin/statusRM.sh
@@ -0,0 +1,5 @@
+echo "/usr/bin/yarn rmadmin -getServiceState rm01"
+      /usr/bin/yarn rmadmin -getServiceState rm01
+
+echo "/usr/bin/yarn rmadmin -getServiceState rm02"
+      /usr/bin/yarn rmadmin -getServiceState rm02
